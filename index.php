570f2347ffa928c5f0ff7c3f1155d7f4f7a6323a
MarcoGPUDC <mgpalavecino@udc.edu.ar>
2025-10-21 10:03:19 -0300

    ajuste de fuente y tamaños para movil

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
   <title>LM</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Poppins&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
   <link rel="icon" type="image/png" href="/favicon.png">
   <link rel="stylesheet" href="/css/index.css">
 </head>
